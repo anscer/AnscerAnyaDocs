b5f54aa911bf3f852345bea47b68408f5c15fcef
Ashwanee Kumar Gupta <ashwanee@anscer.com>
2023-10-12 11:02:52 +0530

    Algolia Search added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const redirects = require("./redirects");
+// const redirects = require("./redirects");
 
 const config = {
   title: "ANSCER ROBOTICS - ANSCER ANYA Documentation",
@@ -127,12 +128,12 @@ const config = {
         id: "docusaurus-plugin-image-zoom",
       },
     ],
-    [
-      "@docusaurus/plugin-client-redirects",
-      {
-        redirects: [...redirects],
-      },
-    ],
+    // [
+    //   "@docusaurus/plugin-client-redirects",
+    //   {
+    //     redirects: [...redirects],
+    //   },
+    // ],
   ],
   themes: ["docusaurus-theme-openapi-docs"],
   themeConfig:
@@ -184,12 +185,32 @@ const config = {
         ],
         hideOnScroll: true,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: process.env.ALGOLIA_APP_ID,
+        // Public API key: it is safe to commit it
+        apiKey: process.env.ALGOLIA_SEARCH_KEY,
+        indexName: "anscer-anya-web",
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: "/docs/",
+        //   // or as RegExp: /\/docs\//
+        //   to: "/",
+        // },
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+      },
       languageTabs: [
         {
-          highlight: "javascript",
-          language: "nodejs",
-          logoClass: "nodejs",
-          variant: "axios",
+          highlight: "bash",
+          language: "curl",
+          logoClass: "bash",
         },
         {
           highlight: "python",
@@ -198,14 +219,15 @@ const config = {
           variant: "requests",
         },
         {
-          highlight: "bash",
-          language: "curl",
-          logoClass: "bash",
-        },
-        {
           highlight: "go",
           language: "go",
           logoClass: "go",
+        },
+        {
+          highlight: "javascript",
+          language: "nodejs",
+          logoClass: "nodejs",
+          variant: "axios",
         },
         {
           highlight: "ruby",
